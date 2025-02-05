7fcf3af4e1caa378b539fd67fe266930fc25f041
elbrocas12 <fanchesso156@hotmail.com>
2025-02-04 23:16:10 -0500

    feat: create animal factory

---
+++ src/main/java/org/example/animal/factory/AnimalFactory.java
@@ -0,0 +1,13 @@
+package org.example.animal.factory;
+
+import org.example.animal.*;
+
+public class AnimalFactory {
+    public static Animal createAnimal(String species, AnimalType type, String onomatopoeia){
+        return switch (type) {
+            case TERRESTRE -> new TerrestrialAnimal(onomatopoeia, species);
+            case ACUATICO -> new AquaticAnimal(onomatopoeia, species);
+            case VOLADOR -> new FlyingAnimal(onomatopoeia, species);
+        };
+    }
+}
